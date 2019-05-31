69d216ae2067efbcc0d7c96cf2178010eac0a4a0
gawainx <liangyixp@live.cn>
2019-05-31 21:29:05 +0800

    Report bugs

---
+++ README.md
@@ -0,0 +1,3 @@
+# debuglsp
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import debuglspTests
+
+var tests = [XCTestCaseEntry]()
+tests += debuglspTests.allTests()
+XCTMain(tests)
